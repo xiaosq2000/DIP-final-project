0706983b18a52d62e0a105201109fa32e3da4690
Shuqi Xiao <xiaosq2000@gmail.com>
2020-12-21 22:02:09 +0800

    Update test_1.cpp

--- xsq_ws/src/ght_pkg/src/test_1.cpp
+++ xsq_ws/src/ght_pkg/src/test_1.cpp
@@ -1,4 +1,9 @@
-// 28行 更改文件路径 
+/* Project: A Self-parking System Based on General Hough Transformation 
+ * 
+ * Author: Shuqi Xiao
+ *
+ * Date: 2020.12.12
+ */
 
 #include <stdlib.h>
 #include <iostream>
@@ -18,13 +23,14 @@
 using namespace std;
 using namespace cv;
 
-// 图像尺寸
+// 图像基础尺寸
 const int kRowNumLowResImg = 200;           //纵坐标最大值
 const int kColNumLowResImg = 200;           //横坐标最大值
 
 // 文件路径
-const string kFilenameTemplate = "/home/zdh/xsq_ws/img/template/template.jpg";
-
+// const string kFilenameTest = "/home/zdh/xsq_ws/img/input/test_alphabet.jpg";
+// const string kFilenameOutput = "/home/zdh/xsq_ws/img/output";
+ 
 Mat HsvSegment(Mat img_origin)
 {
 	Mat img_hsv;
@@ -38,11 +44,12 @@ Mat HsvSegment(Mat img_origin)
 
 Mat PreprocessTemplate(Mat img_origin)
 {
-	//功能：广义霍夫变换模板预处理
-	//输入：模板
-	//输出：预处理后的模板
-	//操作：低分辨率处理；彩色转灰度；高斯滤波；Canny 边沿检测
-
+	/* 功能：广义霍夫变换的图片预处理
+	 * 输入：
+	 * 输出：预处理后的图像
+	 *操作：低分辨率处理；彩色转灰度；高斯滤波；Canny 边沿检测
+	 */
+	// 图像预处理采用的参数
 	const float kGaussianKernSigma = 1.5;      //高斯卷积核方差
 	const float kGaussianKernSize = 5;         //高斯卷积核尺寸
 	const float kCannyThresh1 = 70;            //Canny双阈值处理
@@ -62,19 +69,19 @@ Mat PreprocessTemplate(Mat img_origin)
 
 Mat PreprocessImg(Mat img_origin)
 {
-	/* 功能：广义霍夫变换预处理
-	 * 输入：原图像
+	/* 功能：广义霍夫变换的图片预处理
+	 * 输入：
 	 * 输出：预处理后的图像
-	 * 操作：低分辨率处理；高斯滤波；Canny 边沿检测
-	 */
+	 * 操作：低分辨率处理；彩色转灰度；高斯滤波；Canny 边沿检测
+     */
+	// 图像预处理采用的参数
 	const float kGaussianKernSigma = 1.5;      //高斯卷积核方差
 	const float kGaussianKernSize = 5;         //高斯卷积核尺寸
 	const float kCannyThresh1 = 70;            //Canny双阈值处理
 	const float kCannyThresh2 = 150;           //Canny双阈值处理
 
 	Mat img_low_res;
-	resize(img_origin,img_low_res,cv::Size(kColNumLowResImg, kRowNumLowResImg),CV_INTER_AREA);   
-                                
+	resize(img_origin,img_low_res,cv::Size(kColNumLowResImg, kRowNumLowResImg),CV_INTER_AREA);                                   
 	Mat img_gaussian_blur;
 	GaussianBlur(img_low_res, img_gaussian_blur, cv::Size(kGaussianKernSize,kGaussianKernSize), kGaussianKernSigma);
 	Mat img_canny;
@@ -85,23 +92,22 @@ Mat PreprocessImg(Mat img_origin)
 
 Mat InversePerspectiveMapping(const int (*src_points)[2], const int (*dst_points)[2])
 {
-	/* 功能：单次逆透射变换完成相机的几何畸变校正
+	/* 功能：利用单次逆透射变换完成相机的几何畸变校正
 	 * 输入：参考四边形顶点坐标（二维数组行指针）；变换后矩形四顶点坐标（二维数组行指针）
 	 * 输出：单应矩阵
 	 */
-
-    vector<Point2f> p,q;
-    p.push_back(Point2f(src_points[0][0],src_points[0][1]));
-    q.push_back(Point2f(dst_points[0][0],dst_points[0][1]));
+	vector<Point2f> p,q;
+	
+	p.push_back(Point2f(src_points[0][0],src_points[0][1]));
+	q.push_back(Point2f(dst_points[0][0],dst_points[0][1]));
 	
 	p.push_back(Point2f(src_points[1][0],src_points[1][1]));
-    q.push_back(Point2f(dst_points[1][0],dst_points[1][1]));
-
-    p.push_back(Point2f(src_points[2][0],src_points[2][1]));
+	q.push_back(Point2f(dst_points[1][0],dst_points[1][1]));
+	
+	p.push_back(Point2f(src_points[2][0],src_points[2][1]));
 	q.push_back(Point2f(dst_points[2][0],dst_points[2][1]));
-
-
-    p.push_back(Point2f(src_points[3][0],src_points[3][1]));
+	
+	p.push_back(Point2f(src_points[3][0],src_points[3][1]));
 	q.push_back(Point2f(dst_points[3][0],dst_points[3][1]));
 
 	Mat matrix_perspective_transform = getPerspectiveTransform(p,q);
@@ -115,20 +121,18 @@ int ConstructRTable(Mat img_origin, float (*r_table)[3])
 	 * 输入：模板图片；r_table二维数组的行指针
 	 * 输出：r_table的行数
 	 */
-
 	// 广义霍夫变换之r-table的采样率与阈值
 	const float kAngleSamplingNum = 50;       //角采样率
 	const int kThreshTemplateGrayScale = 100;  //灰度阈值
-
-    // Preprocess
-    Mat img_after_preprocessing;			
-    img_after_preprocessing = PreprocessTemplate(img_origin);
-    // Gradient
-    Mat grad_x, grad_y, grad_mag, grad_ang;				    //x方向梯度, y方向梯度, 梯度模长, 梯度角度
-    Sobel(img_after_preprocessing, grad_x, CV_32FC1, 1, 0); //x方向梯度
-    Sobel(img_after_preprocessing, grad_y, CV_32FC1, 0, 1); //y方向梯度
-    magnitude(grad_x, grad_y, grad_mag);          //梯度幅值
-    phase(grad_x, grad_y, grad_ang, false);       //梯度角（使用弧度制，若true则为角度制
+	// Preprocess
+	Mat img_after_preprocessing;			
+	img_after_preprocessing = PreprocessTemplate(img_origin);
+	// Gradient
+	Mat grad_x, grad_y, grad_mag, grad_ang;				    //x方向梯度, y方向梯度, 梯度模长, 梯度角度
+	Sobel(img_after_preprocessing, grad_x, CV_32FC1, 1, 0); //x方向梯度
+	Sobel(img_after_preprocessing, grad_y, CV_32FC1, 0, 1); //y方向梯度
+	magnitude(grad_x, grad_y, grad_mag);          //梯度幅值
+	phase(grad_x, grad_y, grad_ang, false);       //梯度角（使用弧度制，若true则为角度制
     // xy-table
 	int row_num_r_table = 0;		
 	float xy_table[kRowNumLowResImg*kColNumLowResImg][3];
@@ -151,7 +155,6 @@ int ConstructRTable(Mat img_origin, float (*r_table)[3])
 			}
 		}
 	}
-
 	row_num_r_table++;//得到r-table的行数（模板精细程度的指标，基本由“图像基础尺寸”与“角采样数”决定）
 	// r-table
 	for (int i = 0; i < row_num_r_table; i++)
@@ -188,18 +191,17 @@ int GeneralHoughTransform(Mat img_origin, float (*r_table)[3], int row_num_r_tab
 	magnitude(grad_x, grad_y, grad_mag);     //梯度幅值
 	phase(grad_x, grad_y, grad_ang, false);  //梯度角（使用弧度制，若true则为角度制
 	// Voting
-   	int idx_scale, idx_rotation = 0;//循环变量，遍历模板的尺度与旋转
+	int idx_scale, idx_rotation = 0;//循环变量，遍历模板的尺度与旋转
 	float zoom_factor, angle = 0;//对应的缩放倍数与旋转角度
 	int i,j = 0;//选民的横纵坐标
-   	int x,y = 0;//候选人的横、纵坐标		
+	int x,y = 0;//候选人的横、纵坐标		
 	int counter[kScaleSamplingNum][kRotationSamplingNum][kRowNumLowResImg][kColNumLowResImg] = {0};// 计数器
 	int maximum_counter = 0;							  //最大计数	
 	float zoom_factor_maximum_counter = 0;
 	float angle_maximum_counter = 0;
 	int x_maximum_counter = 0;                            //低分辨率图像中目标的横坐标
 	int y_maximum_counter = 0;                            //低分辨率图像中目标的纵坐标
-
-	for (int idx_scale = 0; idx_scale < kScaleSamplingNum; idx_scale++)
+		for (int idx_scale = 0; idx_scale < kScaleSamplingNum; idx_scale++)
 	{
 		zoom_factor = kMinZoomFactor + idx_scale*(kMaxZoomFactor - kMinZoomFactor)/kScaleSamplingNum;
 		for (int idx_rotation = 0; idx_rotation < kRotationSamplingNum; idx_rotation++)
@@ -222,7 +224,7 @@ int GeneralHoughTransform(Mat img_origin, float (*r_table)[3], int row_num_r_tab
 							//确保(x,y)不能越界
 							if (x >= 0 && x < kColNumLowResImg && y >= 0 && y < kRowNumLowResImg)
 							{
-								counter[idx_scale][idx_rotation][y][x]++;
+									counter[idx_scale][idx_rotation][y][x]++;
 							}
 						}
 					}
@@ -232,10 +234,9 @@ int GeneralHoughTransform(Mat img_origin, float (*r_table)[3], int row_num_r_tab
 	}
     
 	// Sifting
-
     // 利用 std::max_element 找到 counter 最大值 
 	maximum_counter = *std::max_element(&counter[0][0][0][0], &counter[0][0][0][0]+kRowNumLowResImg*kColNumLowResImg*kScaleSamplingNum*kRotationSamplingNum);    
-    // 四层循环找到最大值
+    // 四层循环找到最大值，找到即退出循环。（有待优化）
     bool flag = false;
     for (int idx_scale = 0; idx_scale < kScaleSamplingNum && flag == false; idx_scale++)
     {
@@ -260,7 +261,7 @@ int GeneralHoughTransform(Mat img_origin, float (*r_table)[3], int row_num_r_tab
 
     // 得到原图中的目标点位置
 	x_target = round(x_maximum_counter*img_origin.size().width/kColNumLowResImg);
-    y_target = round(y_maximum_counter*img_origin.size().height/kRowNumLowResImg);
+	y_target = round(y_maximum_counter*img_origin.size().height/kRowNumLowResImg);
 	return maximum_counter; 
 }
 
@@ -273,21 +274,17 @@ Mat Mark(Mat img_origin, int x_target, int y_target)
 	 */
 
 	const int kSizeDisplayBlock = 10;          // 输出图像上目标点处的正方形像素块的大小
-
- 	Mat img_target_display = img_origin.clone();
+	Mat img_target_display = img_origin.clone();
 	//越界检查
 	if (x_target > kSizeDisplayBlock 
 		&& x_target + kSizeDisplayBlock < img_target_display.size().width 
 		&& y_target > kSizeDisplayBlock 
 		&& y_target + kSizeDisplayBlock < img_target_display.size().height)
 	{
-	//	cout<<"0"<<endl;
 		for (int j = y_target - kSizeDisplayBlock; j < y_target + kSizeDisplayBlock; j++)
 		{
-	//		cout<<"1"<<endl;
 			for (int i = x_target - kSizeDisplayBlock; i < x_target + kSizeDisplayBlock; i++)				
 			{
-	//			cout<<"2"<<endl;				
 				img_target_display.at<Vec3b>(j,i)[0] = 255;
 				img_target_display.at<Vec3b>(j,i)[1] = 0;
 				img_target_display.at<Vec3b>(j,i)[2] = 0;
@@ -309,7 +306,7 @@ void Navigate(int x_target, int y_target, int cols, int rows, int poll, bool& fl
 	const float kMinLinearVel = 0.1;
 	const float kMaxLinearVel = 0.3;
 	// 线速度开关控制参数
-	const int kPollThresh1 = 10; // 轻视票数低于此阈值的目标
+	const int kPollThresh1 = 10;// 轻视票数低于此阈值的目标
 	const int kPollThresh2 = 30;// 重视票数高于此阈值的目标
 	// 角速度P控制参数
 	const float kProportion = 0.0020;
@@ -344,7 +341,7 @@ void Navigate(int x_target, int y_target, int cols, int rows, int poll, bool& fl
 			linear_vel = kMaxLinearVel;
 		}
 	}
-	// 停车条件：
+
 	if (rows - y_target < 0.1*rows && poll > kPollThresh1)
 	{
 		flag = 1;
@@ -356,7 +353,9 @@ void Navigate(int x_target, int y_target, int cols, int rows, int poll, bool& fl
 
 int main(int argc, char **argv)
 {
-
+	// 模板图片的路径
+	const string kFilenameTemplate = "/home/zdh/xsq_ws/img/template/template.jpg";
+	// 打开Dashbot摄像头为1，打开笔记本摄像头为0
 	// const int kCamera = 0; 
 	// 几何畸变校正的参考四边形坐标，(x,y)左上、右上、左下、右下
 	const int kQuadrangle[4][2] = {
@@ -370,6 +369,7 @@ int main(int argc, char **argv)
 	{670,0},
 	{0,370},
 	{670,370}};
+
 	// 初始化
 	Mat img_template, img_origin, m_ipm, img_ipm, img_ipm_target_display;
 	float r_table[kRowNumLowResImg*kColNumLowResImg][3] = {0};
@@ -389,20 +389,18 @@ int main(int argc, char **argv)
 	ros::Publisher pub=self_parking.advertise<geometry_msgs::Twist>("/smoother_cmd_vel", 5);
 	// 打开摄像头
 	VideoCapture capture;
-	capture.open(1); // 打开Dashbot摄像头为1，打开笔记本摄像头为0
+	capture.open(1);
 	waitKey(100);
 	if(!capture.isOpened())	// 摄像头异常处理
 	{
 		printf("摄像头图像读取失败。\n");
 	 	return 0;
 	}
-	
 	// 控制量
 	float linear_vel = 0;
 	float angular_vel = 0;
-	// 当flag == 1时，认为已经来到了目标面前
+	// 停车标志：当flag == 1时，认为已经来到了目标面前。
 	bool flag = 0;
-
 	// 主循环
 	while(ros::ok())
 	{
@@ -410,7 +408,7 @@ int main(int argc, char **argv)
 		capture.read(img_origin);
 		img_origin = img_origin(Range(0,img_origin.rows),Range(0,img_origin.cols/2-170));
 		imshow("src",img_origin);			
-		// 颜色分割
+		// 颜色分割（非必要）
 		Mat img_hsv = HsvSegment(img_origin);
 		imshow("img_hsv",img_hsv);
 		// 逆透射变换
@@ -420,7 +418,7 @@ int main(int argc, char **argv)
 		maximum_counter = GeneralHoughTransform(img_ipm, r_table, row_num_r_table, x_target_ipm, y_target_ipm);
 		cout<<"maximum_counter = "<<maximum_counter<<endl;		
 		// 展示识别结果
-	 	img_ipm_target_display = Mark(img_ipm_target_display, x_target_ipm, y_target_ipm);	
+		img_ipm_target_display = Mark(img_ipm_target_display, x_target_ipm, y_target_ipm);	
 		imshow("Target Recognition: After IPM", img_ipm_target_display);	
 		// 导航
 		Navigate(x_target_ipm, y_target_ipm, img_ipm.cols, img_ipm.rows, maximum_counter, flag, linear_vel, angular_vel);
@@ -438,7 +436,7 @@ int main(int argc, char **argv)
 		// 停车
 		if (flag == 1)
 		{
-			for (int i = 0; i<200000; i++)
+			for (int i = 0; i < 200000; i++)
 			{
 				pub.publish(cmd_navigate);
 			}
